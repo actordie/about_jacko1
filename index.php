6a9f5dab87259a3ac34f49a1bf43615153c0278f
S JACKSON <jackson.sabastian@gmail.com>
2017-04-10 11:43:45 -0600

    panic or not

---
+++ index.php
@@ -0,0 +1,12 @@
+
+<html>
+	<head>Focused Wandering</head>
+	<body>
+		<img src="images/walkthisway.jpg">
+		<h6><p>I am in <strong>ABQ</strong></p></h6><
+		<h6><p>10 years</p></h6>
+		<h6><p>I have a love hate relationship with this home away frome</p></h6>
+		<h6><p>Loving learning how to learn</p></h6>
+		<h6><p>I am <strong>SABASTIAN JACKSON</strong> - <em>a failure at best, a learner at worst</em> </p></h6>
+	</body>
+</html>
