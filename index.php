8a6a5da64a426904c2a364643079b7624bb7dc05
amy skidmore <askidmore1@cnm.edu>
2017-04-11 05:32:57 +0900

    .

--- index.php
+++ index.php
@@ -4,10 +4,9 @@
 	<body>
 		<img src="images/walkthisway.jpg">
 		<p>I am in <strong>ABQ</strong>
-		10 years
-		I have a love hate relationship with this home away frome and I really need to wander away from here.
+		10 years. I have a love hate relationship with this home away from and I really need to wander away from here.
 		Loving learning how to learn
 		I am <strong>SABASTIAN JACKSON</strong> -a failure at best, a learner at worst
-</p>
+		</p>
 	</body>
 </html>
